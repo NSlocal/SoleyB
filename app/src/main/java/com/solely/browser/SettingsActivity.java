f34d448613e5fecb3e36845f0369e8349cd16cec
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-25 05:30:29 +0700

    Update SettingsActivity.java

--- app/src/main/java/com/solely/browser/SettingsActivity.java
+++ app/src/main/java/com/solely/browser/SettingsActivity.java
@@ -1,15 +1,25 @@
 package com.solely.browser;
 
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        findViewById(R.id.btn_flags).setOnClickListener(v ->
-            startActivity(getPackageManager().getLaunchIntentForPackage(getPackageName())
-                .setData(android.net.Uri.parse("solely://flags"))));
+
+        findViewById(R.id.btn_flags).setOnClickListener(v -> {
+            startActivity(new Intent(this, FlagsActivity.class));
+        });
+
+        TextView verText = findViewById(R.id.app_version);
+        if (verText != null) {
+            verText.setText("Solely Browser v1.0.36");
+        }
     }
 }
