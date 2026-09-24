d8222b577eb385288c99b122eb6872bdb81175f9
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-25 06:05:25 +0700

    Update ExtensionBridge.java

--- app/src/main/java/com/solely/browser/ExtensionBridge.java
+++ app/src/main/java/com/solely/browser/ExtensionBridge.java
@@ -4,13 +4,20 @@ import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 public class ExtensionBridge {
+    @SuppressWarnings("unused")
     private final WebView webView;
 
-    public ExtensionBridge(WebView wv) { this.webView = wv; }
+    public ExtensionBridge(WebView wv) {
+        this.webView = wv;
+    }
 
     @JavascriptInterface
-    public String getPlatform() { return "solely-browser-android"; }
+    public String getPlatform() {
+        return "solely-browser-android";
+    }
 
     @JavascriptInterface
-    public boolean isMV3Supported() { return true; }
+    public boolean isMV3Supported() {
+        return true;
+    }
 }
